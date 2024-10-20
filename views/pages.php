92d60f2b9299193a9c0f743a81daa8e02a45e455
Satriyo Unggul Wicaksono <stryuw@gmail.com>
2024-10-20 17:08:31 +0700

    Update pages.php

--- views/pages.php
+++ views/pages.php
@@ -1,62 +1,50 @@
 <?php
 require __DIR__ . '/layout/header.php';
 ?>
-<div class="w-full min-h-screen flex items-center justify-center py-28 px-6 gap-10">
-    <div class="w-full flex items-start justify-center gap-10">
-        <div class="max-w-screen-sm w-full items-center flex flex-col justify-center">
+<div class="w-full min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div class="w-full max-w-7xl flex flex-col lg:flex-row items-start justify-center gap-10">
+        <div class="w-full lg:w-1/2 flex flex-col justify-center">
             <form method="post" action="/pages/create">
-                <div class=" space-y-12">
-                    <div class="border-b border-white/10 pb-12">
-                        <h2 class="text-base font-semibold leading-7 text-white">Pages (You have
-                            <?= $countPages  ?>
-                            pages left)</h2>
+                <div class="space-y-8">
+                    <div class="border-b border-white/10 pb-8">
+                        <h2 class="text-base font-semibold leading-7 text-white">Pages (You have <?= $countPages ?> pages left)</h2>
                         <p class="mt-1 text-sm leading-6 text-gray-400">Create a new page or edit an existing page</p>
                         <?php if (!empty($message)): ?>
-                            <div
-                                class="mt-5 sm:mx-auto sm:w-full sm:max-w-sm text-white text-center underline flex mx-auto gap-1 items-center justify-center">
+                            <div class="mt-5 text-white text-center underline flex mx-auto gap-1 items-center justify-center">
                                 <?= $message ?>
                             </div>
                         <?php endif; ?>
-                        <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                            <div class="sm:col-span-6 grid grid-cols-2 gap-x-6 gap-y-8">
-                                <div class="">
-                                    <label for="title"
-                                        class="block text-sm font-medium leading-6 text-white">Title</label>
-                                    <div class="mt-2">
-                                        <div
-                                            class="flex rounded-md bg-white/5 ring-1 ring-inset ring-white/10 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
-                                            <input type="text" name="title" id="title" autocomplete="title"
-                                                class="flex-1 border-0 bg-transparent text-white focus:ring-0 sm:text-sm sm:leading-6"
-                                                placeholder="Satriyo U. Wicaksono">
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="">
-                                    <label for="slug"
-                                        class="block text-sm font-medium leading-6 text-white">Slug</label>
-                                    <div class="mt-2">
-                                        <div
-                                            class="flex rounded-md bg-white/5 ring-1 ring-inset ring-white/10 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
-                                            <span
-                                                class="flex select-none items-center pl-3 text-gray-500 sm:text-sm bg-white/5 pr-3">cleaf.io/</span>
-                                            <input type="text" name="slug" id="slug" autocomplete="slug"
-                                                class="flex-1 border-0 bg-transparent py-1.5 pl-1 text-white focus:ring-0 sm:text-sm sm:leading-6"
-                                                placeholder="wicaksono">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-span-full">
-                                <label for="description"
-                                    class="block text-sm font-medium leading-6 text-white">Description</label>
+                        <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-4 sm:grid-cols-2">
+                            <div class="sm:col-span-1">
+                                <label for="title" class="block text-sm font-medium leading-6 text-white">Title</label>
                                 <div class="mt-2">
-                                    <textarea id="description" name="description" rows="3"
-                                        class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"></textarea>
+                                    <div class="flex rounded-md bg-white/5 ring-1 ring-inset ring-white/10 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
+                                        <input type="text" name="title" id="title" autocomplete="title"
+                                            class="flex-1 border-0 bg-transparent py-1.5 pl-2 text-white focus:ring-0 sm:text-sm sm:leading-6"
+                                            placeholder="Satriyo U. Wicaksono">
+                                    </div>
                                 </div>
-                                <p class="mt-3 text-sm leading-6 text-gray-400">Write a few sentences about your page.
-                                </p>
                             </div>
+
+                            <div class="sm:col-span-1">
+                                <label for="slug" class="block text-sm font-medium leading-6 text-white">Slug</label>
+                                <div class="mt-2">
+                                    <div class="flex rounded-md bg-white/5 ring-1 ring-inset ring-white/10 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
+                                        <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm bg-white/5 pr-3">cleaf.io/</span>
+                                        <input type="text" name="slug" id="slug" autocomplete="slug"
+                                            class="flex-1 border-0 bg-transparent py-1.5 pl-1 text-white focus:ring-0 sm:text-sm sm:leading-6"
+                                            placeholder="wicaksono">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="mt-4">
+                            <label for="description" class="block text-sm font-medium leading-6 text-white">Description</label>
+                            <div class="mt-2">
+                                <textarea id="description" name="description" rows="3"
+                                    class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"></textarea>
+                            </div>
+                            <p class="mt-3 text-sm leading-6 text-gray-400">Write a few sentences about your page.</p>
                         </div>
                     </div>
                 </div>
@@ -65,26 +53,23 @@ require __DIR__ . '/layout/header.php';
                     <button type="button" class="text-sm font-semibold leading-6 text-white">Cancel</button>
                     <button type="submit"
                         class="<?php if ($countPages == 0) : ?> opacity-50 grayscale cursor-not-allowed <?php endif ?>rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-                        <?php if ($countPages == 0) : ?> disabled <?php endif ?>>Add
-                        Page</button>
+                        <?php if ($countPages == 0) : ?> disabled <?php endif ?>>Add Page</button>
                 </div>
             </form>
         </div>
-        <div class="max-w-screen-md w-full">
+        <div class="w-full lg:w-1/2 mt-10 lg:mt-0">
             <?php if (!empty($pages)) : ?>
-                <div class="mx-auto grid grid-cols-1 gap-x-8 gap-y-8 lg:mx-0 lg:grid-cols-2 text-white">
+                <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 text-white">
                     <?php foreach ($pages as $key => $page) : ?>
-                        <?php if ($key > 3) : break;
-                        endif ?>
+                        <?php if ($key > 3) : break; endif ?>
                         <div class="relative group">
                             <a href="/<?= $page['slug'] ?>/content" class="block h-full">
-                                <article
-                                    class="flex flex-col items-start justify-between border border-gray-700 group-hover:border-gray-500 rounded-md p-5 h-full min-h-[100px]">
+                                <article class="flex flex-col items-start justify-between border border-gray-700 group-hover:border-gray-500 rounded-md p-4 h-full">
                                     <div class="w-full flex flex-col h-full">
                                         <h3 class="text-lg font-semibold leading-6 group-hover:text-gray-300">
                                             <?= $page['title'] ?>
                                         </h3>
-                                        <span class="text-sm leading-6 text-gray-400 mb-3">cleaf.io/<?= $page['slug'] ?></span>
+                                        <span class="text-sm leading-6 text-gray-400 mb-2">cleaf.io/<?= $page['slug'] ?></span>
                                         <p class="text-sm leading-6 flex-grow overflow-hidden line-clamp-3">
                                             <?= $page['description'] ?>
                                         </p>
@@ -111,8 +96,6 @@ require __DIR__ . '/layout/header.php';
                 </div>
             <?php endif; ?>
         </div>
-
-
     </div>
 </div>
 
